7f462c6e2b674dcfd1f7f60b7d8885174cbccdff
jxkimathi <judescriva8@gmail.com>
2025-01-15 21:36:22 +0300

    Create the product model

---
+++ backend/src/models/productModel.js
@@ -0,0 +1,39 @@
+// Product schema with fields like name, price, description, stock
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide a product name']
+    },
+
+    price: {
+      type: Number,
+      required: [true, 'Please provide a product price'],
+      min: [0, 'Price cannot be less than 0']
+    },
+
+    description: {
+      type: String,
+      required: [true, 'Please provide a product description'],
+      maxLength: [500, 'Description cannot be more than 500 characters']
+    },
+
+    category: {
+      type: String,
+      required: [true, 'Please provide a product category']
+    },
+
+    stock: {
+      type: Boolean,
+      required: [true, 'Please provide a product stock'],
+      min: [0, 'Stock cannot be less than 0']
+    }
+  }
+);
+
+const productModel = mongoose.model('Product', productSchema);
+
+module.exports = productModel;
+  
