23bc5124a84803a1e3e026ce80773cc14507648c
jxkimathi <judescriva8@gmail.com>
2025-01-21 09:49:35 +0300

    Add the bcrypt import and next function to iterate the next middleware

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,5 +1,6 @@
 // Create the User Model Schema
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
@@ -33,10 +34,13 @@ const userSchema = new mongoose.Schema(
 // Encrypt password using bcrypt
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) {
-    next();
+    return next();
   }
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
+  
 });
 
 const userModel = mongoose.model('User', userSchema);
