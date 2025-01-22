505ada350b1cd1e93ea4087fdc2e4be6ed393e04
jxkimathi <judescriva8@gmail.com>
2025-01-22 17:34:56 +0300

    Add the admin registration and authentiication routes

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,12 +1,15 @@
 // Authentication Routes (/login, /signup)
 const express = require("express");
-const { signUp, login } = require("../controllers/authControllers");
+const { signUp, login, registerAdmin } = require("../controllers/authControllers");
 const authenticate = require("../middleware/auth");
+const isAdmin = require("../middleware/admin");
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/login", login);
+router.post("/register-admin", registerAdmin);
+router.get("/is-admin", authenticate, isAdmin);
 router.get("/authenticate", authenticate);
 
 module.exports = router;
