a7827d173d006e399a07a3c11cd32f49aa66104e
jxkimathi <judescriva8@gmail.com>
2025-01-22 17:43:51 +0300

    Modify product CRUD routes to include user and admin authentication

--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const { readProducts, readProduct, createProduct, updateProduct, deleteProduct } = require("../controllers/productControllers");
-const authenticate = require("../middleware/auth");
-const admin = require("../middleware/admin");
+const isAdmin = require("../middleware/admin");
+const authenticate = require("../middleware/auth")
 
 const router = express.Router();
 
 // CRUD Product routes
 router.get("/fetch", readProducts);
 router.get("/fetch/:id", readProduct);
-router.post("/create", createProduct);
-router.put("/update/:id", authenticate, admin, updateProduct);
-router.delete("/delete/:id",authenticate, admin, deleteProduct);
+router.post("/create", authenticate, isAdmin, createProduct);
+router.put("/update/:id", authenticate,  isAdmin, updateProduct);
+router.delete("/delete/:id", authenticate, isAdmin, deleteProduct);
 
 module.exports = router;
